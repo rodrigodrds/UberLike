366397bfbac75da49a884a367044c0bb9ce5cb8e
Rodrigo Dias Ribeiro da Silva <rodrigo.drds@gmail.com>
2016-12-30 15:23:25 -0200

    Aula 5

--- app/components/map/map.ts
+++ app/components/map/map.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Loading, NavController } from 'ionic-angular'
+import { Geolocation } from 'ionic-native';
+import { Observable } from 'rxjs/Observable';
 
 declare var google: any;
 
@@ -12,11 +15,57 @@ export class MapDirective implements OnInit {
 
   public map;
 
-  constructor() {
+  constructor(public nav: NavController) {
   }
   
   ngOnInit(){
     this.map = this.createMap();
+
+    this.getCurrentLocation().subscribe(location => {
+      this.centerLocation(location);
+    });
+  }
+
+  getCurrentLocation(){
+
+    let loading = Loading.create({
+      content: 'Locating...'
+    });
+
+    this.nav.present(loading);
+
+    let options = {timeout: 10000, enableHighAccuracy: true};
+
+    let locationObs = Observable.create(observable => {
+      Geolocation.getCurrentPosition(options)
+        .then(resp => {
+          let lat = resp.coords.latitude;
+          let lng = resp.coords.longitude;
+
+          let location = new google.maps.LatLng(lat, lng);
+
+          observable.next(location);
+
+          loading.dismiss();
+        },
+        (err)=>{
+          console.log('Geolocation err: ' + err);
+          loading.dismiss();
+        })
+    })
+
+    return locationObs;
+    
+  }
+
+  centerLocation(location){
+    if(location){
+      this.map.panTo(location);
+    }else{
+      this.getCurrentLocation().subscribe(currentLocation => {
+        this.map.panTo(currentLocation);
+      })
+    }
   }
 
   createMap(location = new google.maps.LatLng(-23.609965, -46.927977)){
