4a863768b64b31a9d1443af69a4de0a3649d5325
Rodrigo Dias Ribeiro da Silva <rodrigodrds@users.noreply.github.com>
2017-01-02 18:53:48 -0200

    teste 2

--- app/app.ts
+++ app/app.ts
@@ -13,7 +13,7 @@ export class MyApp {
   constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need. 
+      // Here you can do any higher level native things you might need. teste 
       StatusBar.styleDefault();
     });
   }
